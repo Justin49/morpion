79c74ce8d1b39cf42193b1b74f7e1167d7f4fadf
Justin49 <justin.bell@hotmail.fr>
2020-12-24 17:11:30 +0100

    Création de l'évènement sur les touches directionnelles

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 // Création des constantes
-const PIXEL = 100;
+const PIXEL = 100;// taille du pixel
 const LIGNE = canvas.width/PIXEL;
 const COLONNE = canvas.height/PIXEL;
+const ROND = 0;// correspond au rond traditionnel, ici c'est 0
+const CROIX = 1;// correspond à la croix traditionnel, ici c'est 1
 
 // fonction qui va créer le tableau du morpion
 function créerTableau() {
@@ -42,6 +44,49 @@ function dessinerTableau(tableau) {
 }
 
 dessinerTableau(tableau);
+
+// création de l'évènement qui permettera de bouger à l'intérieur de la grille
+document.addEventListener('keydown', deplacementDansGrille);
+
+var vitesseDeplacementGrilleHorizontal = 0;
+var vitesseDeplacementGrilleVertical = 0;
+
+function deplacementDansGrille(evt) {
+
+    switch(evt.keyCode) {
+
+        case 37:
+            // touche gauche
+            vitesseDeplacementGrilleHorizontal = -PIXEL;
+            vitesseDeplacementGrilleVertical = 0;
+            console.log('gauche');
+            break;
+        case 38:
+            // touche haut
+            vitesseDeplacementGrilleHorizontal = 0
+            vitesseDeplacementGrilleVertical = -PIXEL;
+            console.log('haut');
+            break;
+        case 39:
+            // touche droite
+            vitesseDeplacementGrilleHorizontal = PIXEL;
+            vitesseDeplacementGrilleVertical = 0;
+            console.log('droite');
+            break;
+        case 40:
+            // touche bas
+            vitesseDeplacementGrilleHorizontal = 0;
+            vitesseDeplacementGrilleVertical = PIXEL;
+            console.log('bas');
+            break;
+        case 32:
+            // touche espace
+            vitesseDeplacementGrilleHorizontal = 0;
+            vitesseDeplacementGrilleVertical = 0;
+            break;
+    }
+}
+
 
 
 
